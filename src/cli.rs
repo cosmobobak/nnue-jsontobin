7118a6be755feac25fbf0c6b475e97ff8917114e
Cosmo Bobak <cosmobobak@gmail.com>
2023-07-11 19:38:53 +0100

    remove non-unified option

--- src/cli.rs
+++ src/cli.rs
@@ -5,12 +5,9 @@ use clap::Parser;
 #[allow(clippy::struct_excessive_bools, clippy::option_option)]
 pub struct Cli {
     pub input: Option<std::path::PathBuf>,
-    /// Output path for a unified converted model (use this if you want a single NNUE binary!)
+    /// Output path for the network binary.
     #[clap(short, long, value_name = "PATH")]
-    pub unified: Option<std::path::PathBuf>,
-    /// Output directory for a split converted model.
-    #[clap(short, long, value_name = "PATH")]
-    pub split: Option<std::path::PathBuf>,
+    pub output: Option<std::path::PathBuf>,
     /// The first quantisation parameter.
     #[clap(long, value_name = "K", default_value = "255")]
     pub qa: i32,
